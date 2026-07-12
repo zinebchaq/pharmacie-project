ddf97ab68f459c1f887dfa0f2e2641faac9e6513
zinebchaq <zinebchakchak75@gmail.com>
2026-07-12 21:19:22 +0200

    fix export API_BASE_URL

---
+++ pharmacie-frontend/src/config/api.ts
@@ -0,0 +1,4 @@
+// URL de base de l'API
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+
+export { API_BASE_URL };
--- pharmacie-frontend/src/pages/auth/Login.tsx
+++ pharmacie-frontend/src/pages/auth/Login.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import API_BASE_URL from "../../config/api";
+import { API_BASE_URL } from '../../config/api';
 import styles from "./Login.module.css";
 import pharmacyVideo from "./video.mp4"; 
 
