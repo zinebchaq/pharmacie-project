cc36ccb308fb29f42c86b5626027097638eaf98f
zinebchaq <zinebchakchak75@gmail.com>
2026-07-12 20:57:16 +0200

    ajout proxy vite pour connexion backend

--- pharmacie-frontend/vite.config.ts
+++ pharmacie-frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',  // nom du service Docker
+        changeOrigin: true,
+      }
+    }
+  }
 })
